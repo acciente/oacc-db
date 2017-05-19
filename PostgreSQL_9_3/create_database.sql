ba9983787a737266fc70918a666fb4293805d0de
Fabian Spinnenhirn <fabian@acciente.com>
2017-05-18 19:27:01 -0700

    updates copyright notice in all scripts to be consistent with MySQL compatible comments and updates copyright year in scripts and notice.txt

--- PostgreSQL_9_3/create_database.sql
+++ PostgreSQL_9_3/create_database.sql
@@ -1,5 +1,5 @@
---------------------------------------------------------
--- Copyright 2009-2016, Acciente LLC
+-- -----------------------------------------------------
+-- Copyright 2009-2017, Acciente LLC
 --
 -- Acciente LLC licenses this file to you under the
 -- Apache License, Version 2.0 (the "License"); you
@@ -14,7 +14,7 @@
 -- OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing
 -- permissions and limitations under the License.
---------------------------------------------------------
+-- -----------------------------------------------------
 
 -- OPTIONAL:
 -- create dedicated database for oacc
